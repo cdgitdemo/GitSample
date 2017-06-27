d93760d40b09cc1ce93b8dee697c5529af51f902
JYang <JYang@JYang-LT.cleverdevices.com>
2017-06-27 00:01:39 -0400

    Initial Changes.

---
+++ GitSample/src/HelloWorld.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author JYang
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * 
+	 */
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
