b23ff57178488124cd0ed2b3d7cb50203defa048
Jackson <Jackson@Bustime-VM8-SL>
2017-07-01 23:45:40 -0400

    Add the main method of the EchoTest.

--- GitSample/src/com/cd/git/demo/EchoTest.java
+++ GitSample/src/com/cd/git/demo/EchoTest.java
@@ -26,8 +26,7 @@ public class EchoTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		System.out.print((new EchoTest("Jackson")).echo());		
 	}
 
 }
