cb81e5240bf3e4a78b60f442b062905f7a833e24
JYang <JYang@JYang-LT.cleverdevices.com>
2017-07-02 17:36:37 -0400

    Append java comment to the hello method.

--- GitSample/src/com/cd/git/demo/HelloWorldIF.java
+++ GitSample/src/com/cd/git/demo/HelloWorldIF.java
@@ -4,10 +4,16 @@ package com.cd.git.demo;
  */
 
 /**
- * @author JYang
+ * @author Jackson
  *
  */
 public interface HelloWorldIF {
+	/**
+	 * Say hello to the name given.
+	 * 
+	 * @param name the name to say hello.
+	 * @return the hello greeting.
+	 */
 	public String hello(String name);
 
 }
