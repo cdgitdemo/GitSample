5c036df24c340c03615d76c866d3ad5f802b90da
JYang <JYang@JYang-LT.cleverdevices.com>
2017-07-06 12:52:09 -0400

    Initial Change.

---
+++ GitSample/src/com/cd/git/demo/HelloFemi.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.cd.git.demo;
+
+/**
+ * @author JYang
+ *
+ */
+public class HelloFemi {
+
+	/**
+	 * 
+	 */
+	public HelloFemi() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
