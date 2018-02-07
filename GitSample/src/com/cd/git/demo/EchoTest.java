b34d488811aa0db846b8db77d9baf4bf4bc71ee5
Jackson Yang <jyang@CleverDevices.com>
2018-02-06 23:05:45 -0500

    Added extra line EchoTests.

--- GitSample/src/com/cd/git/demo/EchoTest.java
+++ GitSample/src/com/cd/git/demo/EchoTest.java
@@ -26,7 +26,9 @@ public class EchoTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.print((new EchoTest("Jackson Yang")).echo());
+		System.out.print((new EchoTest("Jianshan Yang, Liu Qian")).echo());
+		System.out.print((new EchoTest("Kai Yang, Wang Damao")).echo());
+		System.out.print((new EchoTest("Jack Yang, Che Xiao")).echo());
 	}
 
 }
