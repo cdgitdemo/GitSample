07d94d74994909fa779b8f6428d1df670aa71f9e
Jackson Yang <jyang@CleverDevices.com>
2018-02-06 23:05:45 -0500

    Added two EchoTests.

--- GitSample/src/com/cd/git/demo/EchoTest.java
+++ GitSample/src/com/cd/git/demo/EchoTest.java
@@ -26,7 +26,9 @@ public class EchoTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.print((new EchoTest("Jackson Yang")).echo());
+		System.out.print((new EchoTest("Jianshan Yang")).echo());
+		System.out.print((new EchoTest("Kai Yang")).echo());
+		System.out.print((new EchoTest("Jack Yang")).echo());
 	}
 
 }
